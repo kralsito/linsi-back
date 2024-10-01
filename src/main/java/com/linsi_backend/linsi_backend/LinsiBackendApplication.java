e497028afe350805937159af8e96d5022fd66eda
kralsito <franco_kral@hotmail.com>
2024-10-01 00:32:01 -0300

    inicio el proyectardo

---
+++ src/main/java/com/linsi_backend/linsi_backend/LinsiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linsi_backend.linsi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinsiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinsiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linsi_backend/linsi_backend/LinsiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linsi_backend.linsi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinsiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
